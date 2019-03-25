df089d0081c171b4107a469ab38575b7eeeebc4b
Nabil Kashyap <nkashya1@swarthmore.edu>
2019-03-25 13:48:11 -0400

    adds category js

--- javascripts/holemp.js
+++ javascripts/holemp.js
@@ -1,1 +1,30 @@
-console.log('fire holemp')
+/*
+
+holidays in the empire - berlin child theme
+https://ds-exhibits.swarthmore.edu/holidays-in-the-empire
+neatline emoji helper
+
+*/
+
+const emoji = {
+guidebooks: 'books.png',
+geography: 'mountain.png',
+people: 'bust_in_silhouette.png',
+religion: 'mosque.png',
+climate: 'sunny.png',
+cities: 'house-buildings',
+culture: 'crescent_moon.png',
+economy: 'monybag.png',
+encounters: 'eye.png'
+}
+
+const baseUrl = 'https://ds-exhibits.swarthmore.edu/holidays-in-the-empire/themes/theme-holemp/emoji/'
+
+emoji.map( function(value, key) {
+
+    $.each($('[*|href="' + key + '"]'), function(i,e) { 
+        $(e).attr('xlink:href', baseUrl + value) 
+    })
+
+});
+
