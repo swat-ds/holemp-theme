94e913d8ad98921d949abe912c14c67ceb7ff06f
Nabil Kashyap <nkashya1@swarthmore.edu>
2019-05-24 15:38:34 -0400

    adds favicons

--- common/header.php
+++ common/header.php
@@ -24,6 +24,14 @@
 
     echo head_css();
     ?>
+
+    <!-- Icons -->
+    <link rel="icon" type="image/png" href="/favicon-192x192.png" sizes="192x192">
+    <link rel="icon" type="image/png" href="/favicon-160x160.png" sizes="160x160">
+    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+
     <!-- JavaScripts -->
     <?php queue_js_file('vendor/selectivizr', 'javascripts', array('conditional' => '(gte IE 6)&(lte IE 8)')); ?>
     <?php queue_js_file('vendor/respond'); ?>
