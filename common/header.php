9fc406b241a4e74cb73e97b39e7c38e46b574ba2
Nabil Kashyap <nkashya1@swarthmore.edu>
2019-05-24 15:41:06 -0400

    updates favicon paths

--- common/header.php
+++ common/header.php
@@ -26,11 +26,11 @@
     ?>
 
     <!-- Icons -->
-    <link rel="icon" type="image/png" href="/favicon-192x192.png" sizes="192x192">
-    <link rel="icon" type="image/png" href="/favicon-160x160.png" sizes="160x160">
-    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
-    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
-    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/themes/theme-holemp/favicon-192x192.png" sizes="192x192">
+    <link rel="icon" type="image/png" href="/themes/theme-holemp/favicon-160x160.png" sizes="160x160">
+    <link rel="icon" type="image/png" href="/themes/theme-holemp/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/png" href="/themes/theme-holemp/favicon-16x16.png" sizes="16x16">
+    <link rel="icon" type="image/png" href="/themes/theme-holemp/favicon-32x32.png" sizes="32x32">
 
     <!-- JavaScripts -->
     <?php queue_js_file('vendor/selectivizr', 'javascripts', array('conditional' => '(gte IE 6)&(lte IE 8)')); ?>
