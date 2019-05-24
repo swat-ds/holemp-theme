fcba3621965af829f5c71f58c3f15e01c5d77450
Nabil Kashyap <nkashya1@swarthmore.edu>
2019-05-24 15:17:53 -0400

    updates header matomo

--- common/header.php
+++ common/header.php
@@ -39,9 +39,9 @@
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
-    var u="//107.21.4.14/matomo/";
+    var u="//example.com/matomo/";
     _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '2']);
+    _paq.push(['setSiteId', 'example site Id']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
   })();
